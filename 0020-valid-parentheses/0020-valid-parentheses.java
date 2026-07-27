f6a349b9dd104ddc36683be4c475c466dc81dd8d
Aaditya Sengar <theaadityasengarr@gmail.com>
2026-07-27 11:50:37 +0530

    Time: 6 ms (5.48%), Space: 43.6 MB (10.24%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,29 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '[' || ch == '{') {
-                stack.push(ch);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                char top = stack.pop();
-                if (ch == ')' && top != '(') {
-                    return false;
-                }
-                if (ch == ']' && top != '[') {
-                    return false;
-                }
-                if (ch == '}' && top != '{') {
-                    return false;
-                }
+        // Create hashmap to store the pairs...
+        HashMap<Character, Character> Hmap = new HashMap<Character, Character>();
+        Hmap.put(')','(');
+        Hmap.put('}','{');
+        Hmap.put(']','[');
+        // Create stack data structure...
+        Stack<Character> stack = new Stack<Character>();
+        // Traverse each charater in input string...
+        for (int idx = 0; idx < s.length(); idx++){
+            // If open parentheses are present, push it to stack...
+            if (s.charAt(idx) == '(' || s.charAt(idx) == '{' || s.charAt(idx) == '[') {
+                stack.push(s.charAt(idx));
+                continue;
+            }
+            // If the character is closing parentheses, check that the same type opening parentheses is being pushed to the stack or not...
+            // If not, we need to return false...
+            if (stack.size() == 0 || Hmap.get(s.charAt(idx)) != stack.pop()) {
+                return false;
             }
         }
-        return stack.isEmpty();
+        // If the stack is empty, return true...
+        if (stack.size() == 0) {
+            return true;
+        }
+        return false;
     }
 }
