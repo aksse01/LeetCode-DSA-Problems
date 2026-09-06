5b678b9635b6a99600ebc10411ff41b9a3eb60d6
Aaditya Sengar <theaadityasengarr@gmail.com>
2026-09-06 14:02:06 +0530

    Time: 14 ms (92.72%), Space: 54.2 MB (73.01%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,62 +1,24 @@
 class Solution {
-    private int count(int i, int j, String s, String t, int[][] memo) {
-        if (j == 0) return 1;
-        if (i == 0) return 0;
-        if (memo[i][j] != -1) return memo[i][j];
-
-        memo[i][j] = count(i - 1, j, s, t, memo);
-        if (s.charAt(i - 1) == t.charAt(j - 1)) {
-            memo[i][j] += count(i - 1, j - 1, s, t, memo);
-        }
-        return memo[i][j];
-    }
-
     public int numDistinct(String s, String t) {
-        // Top-down
-        // int m = s.length(), n = t.length();
-        // if (n > m) return 0;
-        // int[][] memo = new int[m + 1][n + 1];
-        // for (int[] row: memo) {
-        //     Arrays.fill(row, -1);
-        // }
-        // return count(m, n, s, t, memo);
-
-
-        // // Bottom-up 2D
-        // int m = s.length(), n = t.length();
-        // if (n > m) return 0;
-        // int[][] dp = new int[m + 1][n + 1];
-        // for (int i = 0; i <= m; ++i) {
-        //     dp[i][0] = 1;
-        // }
-        // for (int j = 1; j <= n; ++j) {
-        //     dp[0][j] = 0;
-        // }
-        // for (int i = 1; i <= m; ++i) {
-        //     for (int j = 1; j <= n; ++j) {
-        //         dp[i][j] = dp[i - 1][j];
-        //         if (s.charAt(i - 1) == t.charAt(j - 1)) {
-        //             dp[i][j] += dp[i-1][j-1];
-        //         }
-        //     }
-        // }
-        // return dp[m][n];
-
-        // Bottom-up 1D + prevDiag
         int m = s.length(), n = t.length();
-        if (n > m) return 0;
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        for (int i = 1; i <= m; ++i) {
-            int prevDiag = dp[0];
-            for (int j = 1; j <= n; ++j) {
-                int temp = dp[j];
-                if (s.charAt(i - 1) == t.charAt(j - 1)) {
-                    dp[j] += prevDiag;
+        if (m < n) {
+            return 0;
+        }
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            dp[i][n] = 1;
+        }
+        for (int i = m - 1; i >= 0; i--) {
+            char sChar = s.charAt(i);
+            for (int j = n - 1; j >= 0; j--) {
+                char tChar = t.charAt(j);
+                if (sChar == tChar) {
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+                } else {
+                    dp[i][j] = dp[i + 1][j];
                 }
-                prevDiag = temp;
             }
         }
-        return dp[n];
+        return dp[0][0];
     }
 }
